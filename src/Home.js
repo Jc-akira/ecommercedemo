0aabcc55d58fd0dc257da63b948cf52841c4d36b
Jc-akira <79148861+Jc-akira@users.noreply.github.com>
2021-02-17 14:39:53 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     return(
    
      <div>
-       <img src = "/public/ecommerce-2140604_1920.jpg"></img>
+       <img src = "ecommercedemo/public/ecommerce-2140604_1920.jpg"></img>
         
      </div>
     );
