3a4ddf3d339b9e02f97be7a3239256dd9725a463
Jc-akira <79148861+Jc-akira@users.noreply.github.com>
2021-02-17 15:37:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ const App = () => {
    <>
    <Menu />
    <Switch>
-     <Route exact path="/ecommercedemo/home" component={Home} />  
-     <Route exact path="/ecommercedemo/about" component={About} />  
-     <Route  path="/ecommercedemo/shop" component={Shop} /> 
-     <Route  path="/ecommercedemo/contact" component={Contact} /> 
+     <Route exact path="/" component={Home} />  
+     <Route exact path="/about" component={About} />  
+     <Route  path="/shop" component={Shop} /> 
+     <Route  path="/contact" component={Contact} /> 
    </Switch>
 
    </>
